30ce84be9e943095d18d858e7956663b74d6deb3
Ahmad Aboelghet <ahmed.g@algorithms.sa>
2026-01-11 15:20:37 +0200

    fix bug

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,26 +1,29 @@
+// firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
-// إعدادات الفايربيس (نفس اللي في صفحة parent.html)
+// إعدادات مشروعك (نفس اللي في script.js)
 firebase.initializeApp({
   apiKey: "AIzaSyAbN4awHvNUZWC-uCgU_hR7iYiHk-3dpv8",
   authDomain: "learnaria-483e7.firebaseapp.com",
   projectId: "learnaria-483e7",
   storageBucket: "learnaria-483e7.firebasestorage.app",
   messagingSenderId: "573038013067",
-  appId: "1:573038013067:web:db6a78e8370d33b07a828e"
+  appId: "1:573038013067:web:db6a78e8370d33b07a828e",
+  measurementId: "G-T68CEZS4YC"
 });
 
 const messaging = firebase.messaging();
 
-// التعامل مع الإشعارات أثناء عمل التطبيق في الخلفية
+// التعامل مع الإشعارات في الخلفية
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: './favicon.png' // تأكد إن مسار الأيقونة صح
+    icon: '/assets/images/favicon.png', // تأكد إن اللوجو موجود هنا
+    badge: '/assets/images/favicon.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
