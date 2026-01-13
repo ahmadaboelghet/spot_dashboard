3fe30e8a4affe3184be4805249a65a45125a9ee4
Ahmad Aboelghet <ahmed.g@algorithms.sa>
2026-01-13 02:26:01 +0200

    fix bugs

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,27 @@
 importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyAbN4awHvNUZWC-uCgU_hR7iYiHk-3dpv8",
   authDomain: "learnaria-483e7.firebaseapp.com",
   projectId: "learnaria-483e7",
   storageBucket: "learnaria-483e7.firebasestorage.app",
   messagingSenderId: "573038013067",
   appId: "1:573038013067:web:db6a78e8370d33b07a828e"
-});
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
+// التعامل مع الإشعارات في الخلفية
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: './favicon.png'
+    icon: '/assets/images/learnaria_logo.png' // تأكد من المسار
   };
+
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
