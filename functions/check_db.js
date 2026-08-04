ddc550ddaca427550af3e56158b782f1d5a943c6
Ahmad Abdo <ahmed.abdo@atr-eg.com>
2026-08-04 11:33:33 +0300

    Update Support Phone Number

--- functions/check_db.js
+++ functions/check_db.js
@@ -6,15 +6,15 @@ process.env.FIREBASE_AUTH_EMULATOR_HOST = '127.0.0.1:9090';
 async function test() {
     try {
         const db = admin.firestore();
-        const doc = await db.collection('centers').doc('+201001133246').get();
+        const doc = await db.collection('centers').doc('+201009856266').get();
         if(doc.exists) console.log('Center Data:', doc.data());
         else console.log('Center does not exist');
         
-        const user = await admin.auth().getUserByPhoneNumber('+201001133246');
+        const user = await admin.auth().getUserByPhoneNumber('+201009856266');
         console.log('Auth User:', user);
         
         // Also check getUserByUid
-        const user2 = await admin.auth().getUser('+201001133246');
+        const user2 = await admin.auth().getUser('+201009856266');
         console.log('Auth User by UID:', user2);
     } catch(e) {
         console.error(e);
